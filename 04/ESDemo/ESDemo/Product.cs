1894e450e5f6bff8e133a12d3ce54e4de9add8e3
catcherwong <catcher_hwq@outlook.com>
2019-04-01 22:44:33 +0800

    ES

---
+++ 04/ESDemo/ESDemo/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ESDemo
+{
+    class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ 04/ESDemo/ESDemo/ProductVm.cs
@@ -0,0 +1,13 @@
+﻿namespace ESDemo
+{
+    class ProductVm
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
