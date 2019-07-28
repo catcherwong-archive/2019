184d0595adf037147f30d9f522a6c5bd66ee9225
catcherwong <catcher_hwq@outlook.com>
2019-07-28 22:24:31 +0800

    :sparkles: .net core 3.0 preview 7

---
+++ 07/DotNetCore3Previwe7/GrpcService-CSharp11.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GrpcService_CSharp11.Client
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Grpc.Net.Client;
+    using Greet;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // fixed https error
+            var httpclientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true
+            };
+
+            var httpClient = new HttpClient(httpclientHandler)
+            {
+                // The port number(5001) must match the port of the gRPC server.
+                BaseAddress = new Uri("https://localhost:5001")
+            };
+
+            var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
