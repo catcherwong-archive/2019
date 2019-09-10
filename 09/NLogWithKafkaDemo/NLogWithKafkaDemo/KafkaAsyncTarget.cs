ccea0be7afdbe145a8c6742a6c1a3555adf42504
catcherwong <catcher_hwq@outlook.com>
2019-09-10 22:39:06 +0800

    :ok_hand: update nlog with kafka demo

--- 09/NLogWithKafkaDemo/NLogWithKafkaDemo/KafkaAsyncTarget.cs
+++ 09/NLogWithKafkaDemo/NLogWithKafkaDemo/KafkaAsyncTarget.cs
@@ -16,7 +16,6 @@
     [Target("KafkaAsync")]
     public class KafkaAsyncTarget : AsyncTaskTarget
     {
-        private const char SplitChar = '~';
         private readonly ConcurrentQueue<IProducer<Null, string>> _producerPool;
         private int _pCount;
         private int _maxSize;
@@ -32,6 +31,12 @@
 
         [RequiredParameter]
         public string BootstrapServers { get; set; }
+
+        [RequiredParameter]
+        public Layout TraceId { get; set; }
+
+        [RequiredParameter]
+        public Layout RequestIp { get; set; }
 
         protected override void CloseTarget()
         {
@@ -103,11 +108,11 @@
         protected override async Task WriteAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken)
         {
             var instanceIp = GetCurrentIp();
+
             string topic = this.Topic.Render(logEvent);
-            // traceId~requestIp~message
-            string logMessage = this.Layout.Render(logEvent);
-            
-            var (traceId, requestIp, msg) = GetInfo(logMessage);
+            string traceId = this.TraceId.Render(logEvent);
+            string requestIp = this.RequestIp.Render(logEvent);
+            string msg = this.Layout.Render(logEvent);
 
             var json = JsonConvert.SerializeObject(new
             {
@@ -141,20 +146,6 @@
                     producer.Dispose();
                 }
             }
-        }
-
-        private (string traceId, string requestIp, string msg) GetInfo(string logMessage)
-        {
-            var span = logMessage.AsSpan();
-
-            var fIndex = span.IndexOf(SplitChar);
-            var lIndex = span.LastIndexOf(SplitChar);
-
-            var traceId = fIndex == 0 ? string.Empty : span.Slice(0, fIndex - 1).ToString();
-            var requestIp = lIndex == 1 ? string.Empty : span.Slice(fIndex + 1, lIndex - fIndex - 1).ToString();
-            var msg = span.Slice(lIndex + 1).ToString();
-
-            return (traceId, requestIp, msg);
         }
     }
 }
--- 09/NLogWithKafkaDemo/NLogWithKafkaDemo/KafkaTarget.cs
+++ 09/NLogWithKafkaDemo/NLogWithKafkaDemo/KafkaTarget.cs
@@ -15,7 +15,6 @@
     [Target("Kafka")]
     public class KafkaTarget : TargetWithLayout
     {
-        private const char SplitChar = '~';
         private readonly ConcurrentQueue<IProducer<Null, string>> _producerPool;
         private int _pCount;
         private int _maxSize;
@@ -32,6 +31,12 @@
         [RequiredParameter]
         public string BootstrapServers { get; set; }
 
+        [RequiredParameter]
+        public Layout TraceId { get; set; }
+
+        [RequiredParameter]
+        public Layout RequestIp { get; set; }
+
         protected override void CloseTarget()
         {
             base.CloseTarget();
@@ -45,10 +50,11 @@
         protected override void Write(LogEventInfo logEvent)
         {            
             var instanceIp = GetCurrentIp();
-            string topic = this.Topic.Render(logEvent);
-            string logMessage = this.Layout.Render(logEvent);
 
-            var (traceId, requestIp, msg) = GetInfo(logMessage);
+            string topic = this.Topic.Render(logEvent);
+            string traceId = this.TraceId.Render(logEvent);
+            string requestIp = this.RequestIp.Render(logEvent);
+            string msg = this.Layout.Render(logEvent);
 
             var json = JsonConvert.SerializeObject(new
             {
@@ -141,20 +147,6 @@
             }
 
             return instanceIp;
-        }
-
-        private (string traceId, string requestIp, string msg) GetInfo(string logMessage)
-        {
-            var span = logMessage.AsSpan();
-
-            var fIndex = span.IndexOf(SplitChar);
-            var lIndex = span.LastIndexOf(SplitChar);
-
-            var traceId = fIndex == 0 ? string.Empty : span.Slice(0, fIndex - 1).ToString();
-            var requestIp = lIndex == 1 ? string.Empty : span.Slice(fIndex + 1, lIndex - fIndex - 1).ToString();
-            var msg = span.Slice(lIndex + 1).ToString();
-
-            return (traceId, requestIp, msg);
         }
     }
 }
