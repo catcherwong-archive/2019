09e5f49407b8239409e857f8117877eedb6b0134
catcherwong <catcher_hwq@outlook.com>
2019-11-21 16:37:04 +0800

    :sparkles: multi env for nacos config

---
+++ 11/NacosConfigSample/README.md
@@ -0,0 +1,5 @@
+# NacosConfigSample
+
+Follow this post for more information.
+
+[ASP.NET Core使用Nacos作为配置中心的多环境问题](https://www.cnblogs.com/catcher1994/p/11906556.html)
---
+++ 11/NacosConfigSample/Sample1/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Sample1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddNacos(Configuration);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var nacosConfigClient = app.ApplicationServices.GetRequiredService<Nacos.INacosConfigClient>();
+
+            var val = nacosConfigClient.GetConfigAsync(new Nacos.GetConfigRequest
+            {
+                 DataId = "test"
+            }).GetAwaiter().GetResult();
+
+            Console.WriteLine($"==={env.EnvironmentName}==={val}===");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
